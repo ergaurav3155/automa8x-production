1ea21556bd7e74b11cace1addd36a369b008f5e9
Meet2054 <meetaparekh2003@gmail.com>
2024-08-05 18:05:04 +0530

    inital push

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,12 +1,45 @@
 'use client'
-
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 
 type Props = {}
 
 const MenuOptions = (props: Props) => {
+  const pathName = usePathname()
+
   return (
-    <div>MenuOptions</div>
+    <nav className='dark:bg-black h-screen overflow-scroll justify-between flex flex-col gap-10 py-6 px-2' >
+      <div className='flex items-center justify-center flex-col gap-8' >
+        <Link 
+          className="flex font-bold flex-row"
+          href="/"
+        >
+          Fuzzie
+          </Link>
+          <TooltipProvider> 
+            {menuOptions.map(()=>(
+              <div>
+              <Tooltip>
+              <TooltipTrigger>Hover</TooltipTrigger>
+              <TooltipContent>
+              <p>Add to library</p>
+              </TooltipContent>
+              </Tooltip>
+              </div>
+            ))}
+            </TooltipProvider>
+      </div>
+
+    </nav>
   )
 }
 
